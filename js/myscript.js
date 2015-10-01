e85d75f06cabc99cbf81d1bf6f95c2b1472adbe9
Talbene <talbene@gmail.com>
2015-10-01 17:54:03 +0300

    logo update

--- js/myscript.js
+++ js/myscript.js
@@ -5,10 +5,8 @@ $(function() {
   var topoffset = 50; //variable for menu height
   var slideqty = $('#featured .item').length;
   var wheight = $(window).height(); //get the height of the window
-  var randSlide = Math.floor(Math.random()*slideqty);
 
-  $('#featured .item').eq(randSlide).addClass('active');
-
+  $('#featured .item').eq(0).addClass('active');
 
   $('.fullheight').css('height', wheight); //set to window tallness  
 
@@ -34,34 +32,21 @@ $(function() {
     offset: topoffset
   });
 
-  // add inbody class
  
 
-
-  // Add an inbody class to nav when scrollspy event fires
-  
-
-
   //Use smooth scrolling when clicking on navigation
-  $('.navbar a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') === 
-      this.pathname.replace(/^\//,'') && 
-      location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top-topoffset+2
-        }, 500);
-        return false;
-      } //target.length
-    } //click function
-  }); //smooth scrolling
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $( $(this).attr('href') );
+
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+});
 
   //Automatically generate carousel indicators
   for (var i=0; i < slideqty; i++) {
     var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
-    if (i === randSlide) {
+    if (i === 0) {
       insertText += ' class="active" ';
     }
     insertText += '></li>';
